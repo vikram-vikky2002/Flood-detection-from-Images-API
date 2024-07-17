2d5f5eea8f94b7b5cf0a2a6b1d431b5844beb278
Vikram <mamundivikram@gmail.com>
2024-07-17 21:32:19 +0530

    heroku added

--- app.py
+++ app.py
@@ -52,45 +52,6 @@ async def predict(file: UploadFile = File(...)):
         return JSONResponse(content={'prediction': decoded_prediction})
     except Exception as e:
         return JSONResponse(content={'error': str(e)}, status_code=400)
+
 # To run the app: `uvicorn app:app --reload`
 
-
-
-# from fastapi import FastAPI, File, UploadFile
-# from fastapi.responses import JSONResponse
-# import tensorflow as tf
-# from PIL import Image
-# import numpy as np
-# import io
-
-# # Load your trained model
-# model = tf.keras.models.load_model('fine_tuned_flood_detection_model.h5')
-
-# # Initialize FastAPI app
-# app = FastAPI()
-
-# # Function to preprocess the image
-# def preprocess_image(image: Image.Image) -> np.ndarray:
-#     image = image.resize((224, 224))  # Resize to the expected size
-#     image = np.array(image) / 255.0   # Normalize to [0, 1]
-#     image = np.expand_dims(image, axis=0)  # Add batch dimension
-#     return image
-
-
-
-
-# # Define a prediction endpoint
-# @app.post('/predict')
-# async def predict(file: UploadFile = File(...)):
-#     try:
-#         # Read the image file
-#         image = Image.open(io.BytesIO(await file.read()))
-#         # Preprocess the image
-#         input_data = preprocess_image(image)
-#         # Make prediction
-#         prediction = model.predict(input_data)
-#         return JSONResponse(content={'prediction': prediction.tolist()})
-#     except Exception as e:
-#         return JSONResponse(content={'error': str(e)}, status_code=400)
-
-# # To run the app: `uvicorn app:app --reload`
